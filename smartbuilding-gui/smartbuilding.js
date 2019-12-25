c655a43d9997fbe635f38267d1df337487c7a8f0
Pierre Laurens <plaurens@etud.insa-toulouse.fr>
2019-12-25 22:06:24 +0100

    improvement of GUI: Display Values Sensors and Actuators state

--- smartbuilding-gui/smartbuilding.js
+++ smartbuilding-gui/smartbuilding.js
@@ -1,22 +1,82 @@
 //document.getElementById("roomName").value = "Room02";
 
-function getActuatorStatus(roomName,actuatorName) {
+
+var serverResponse, responsestatus, global;
+/*function getActuatorStatus(roomName,actuatorName) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var serverResponse = JSON.parse(xhttp.responseText);
+		
+		 if(this.readyState == 4 && this.status == 200) {
+			serverResponse = JSON.parse(xhttp.responseText);
 			for (var i in serverResponse) {
-				return serverResponse[i] ;
+				this.responsestatus = serverResponse[i];
+				console.log("In for " + this.responsestatus);
+				document.createTextNode(this.responsestatus);
 			}
-	        }
+		}
 	};
 	xhttp.open("GET", "http://127.0.0.1:8000/getroom/"+roomName+"/readactuator/"+actuatorName, true);
 	xhttp.setRequestHeader("Content-type", "application/json");
 	xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
-	xhttp.send();
+	xhttp.send(null);	
+}*/
+
+function getSensorStatus(roomName,sensorName)
+{   
+	var xhttpSensorStatus = new XMLHttpRequest();
+	xhttpSensorStatus.open("GET", "http://127.0.0.1:8000/getroom/"+roomName+"/readsensor/"+sensorName,  false); 
+	xhttpSensorStatus.setRequestHeader("Content-type", "application/json");
+	xhttpSensorStatus.setRequestHeader("Access-Control-Allow-Origin", "*");
+	xhttpSensorStatus.send(null);
+   
+   serverResponse = JSON.parse(xhttpSensorStatus.responseText);
+		switch (sensorName){
+			case 'Temperature':
+				this.responsestatus = serverResponse.Temperature + " °C";
+				console.log(this.responsestatus);
+			break;
+			case 'Brightness':
+				this.responsestatus = serverResponse.Brightness + " lux";
+				console.log(this.responsestatus);
+			break;
+		}
+		return this.responsestatus;
 }
 
-
+function getActuatorStatus(roomName,actuatorName)
+{   
+	var xhttpactuatorStatus = new XMLHttpRequest();
+	xhttpactuatorStatus.open("GET", "http://127.0.0.1:8000/getroom/"+roomName+"/readactuator/"+actuatorName,  false); 
+	xhttpactuatorStatus.setRequestHeader("Content-type", "application/json");
+	xhttpactuatorStatus.setRequestHeader("Access-Control-Allow-Origin", "*");
+	xhttpactuatorStatus.send(null);
+   
+   serverResponse = JSON.parse(xhttpactuatorStatus.responseText);
+		switch (actuatorName){
+			case 'Hvac':
+				this.responsestatus = serverResponse.Hvac;
+				console.log(this.responsestatus);
+			break;
+			case 'Window':
+				this.responsestatus = serverResponse.Window;
+				console.log(this.responsestatus);
+			break;
+			case 'Lamp':
+				this.responsestatus = serverResponse.Lamp;
+				console.log(this.responsestatus);
+			break;
+			case 'Shutter':
+				this.responsestatus = serverResponse.Shutter;
+				console.log(this.responsestatus);
+				
+			break;
+			case 'Door':
+				this.responsestatus = serverResponse.Door;
+				console.log(this.responsestatus);
+			break;
+		}
+		return this.responsestatus;
+}
 
 function displayRoom(roomName) {
 	document.getElementById("displayRoom").innerHTML = "";
@@ -36,7 +96,7 @@ function displayRoom(roomName) {
 			currentDiv.append(pDesc);
 			for (var i in serverResponse) {
 				var newP = document.createElement("p");
-				newP.append(document.createTextNode(serverResponse[i]));
+				newP.append(document.createTextNode(serverResponse[i]+ " =>" + getSensorStatus(roomName,serverResponse[i])));
 				currentDiv.append(newP);
 			}
 		}
@@ -44,32 +104,33 @@ function displayRoom(roomName) {
 	xhttpsensor.open("GET", "http://127.0.0.1:8000/getroom/"+roomName+"/sensors", true);
 	xhttpsensor.setRequestHeader("Content-type", "application/json");
 	xhttpsensor.setRequestHeader("Access-Control-Allow-Origin", "*");
-	xhttpsensor.send();
+	xhttpsensor.send(null);
 
-
-        var xhttpactuator = new XMLHttpRequest();
+	
+		var xhttpactuator = new XMLHttpRequest();
 	xhttpactuator.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var serverResponse = JSON.parse(xhttpactuator.responseText);
-			var pDesc = document.createElement("p");
-			var bold = document.createElement("b");
-			bold.append("Sensors:");
-			pDesc.append(bold);
-			currentDiv.append(pDesc);
-			for (var i in serverResponse) {
-				var newP = document.createElement("p");
-				newP.append(document.createTextNode(serverResponse[i]+" =>"+getActuatorStatus(roomName,serverResponse[i])));
-				currentDiv.append(newP);
-			}
+		var serverResponse = JSON.parse(xhttpactuator.responseText);
+		var pDesc = document.createElement("p");
+		var bold = document.createElement("b");
+		bold.append("Actuators:");
+		pDesc.append(bold);
+		currentDiv.append(pDesc);
+		for (var i in serverResponse) {
+			var newP = document.createElement("p");
+			newP.append(document.createTextNode(serverResponse[i]+" =>" + getActuatorStatus(roomName,serverResponse[i])));
+			console.log("Actuator" + getActuatorStatus(roomName,serverResponse[i]));
+			currentDiv.append(newP);
 		}
 	};
-	xhttpactuator.open("GET", "http://127.0.0.1:8000/getroom/"+roomName+"/actuators", true);
+	xhttpactuator.open("GET", "http://127.0.0.1:8000/getroom/"+roomName+"/actuators", false);
 	xhttpactuator.setRequestHeader("Content-type", "application/json");
 	xhttpactuator.setRequestHeader("Access-Control-Allow-Origin", "*");
 	xhttpactuator.send();
-
-
 }
+
+
+
+
 
 function addRoomElement(roomName) {
 	var newDiv = document.createElement("div");
@@ -102,7 +163,7 @@ function addRoom() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-	        	// Here call a function that adds a room display
+	        // Here call a function that adds a room display
 			addRoomElement(document.getElementById("roomName").value);
 			displayRoom(document.getElementById("roomName").value);
 	        }
